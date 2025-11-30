d9d561dc64cee1bad5bdf8a6a03e142a51785aef
MarleyS439 <marleysantos439@gmail.com>
2025-11-30 20:34:39 -0300

    Melhoria na visualização de KPIS e gráficos
    - Adição de gráficos faltantes
    - Correção de alguns estilos
    - Melhoria na visualização de algumas KPIS
    - Adição de outras duas KPIS:
    	- Quantidade de setores cadastrados
    	- Quantidade de sensores com algum problema

--- public/assets/javascript/dashboard.js
+++ public/assets/javascript/dashboard.js
@@ -1,9 +1,7 @@
-// Chart.register(window["chartjs-plugin-annotation"]);
-
-//   grafico 2
-// plugin para linha fixa de limite de explosividade
-
 const ctx = document.getElementById("media_setor");
+const ctx1 = document.getElementById("alertas_por_setor");
+const ctx2 = document.getElementById("comparar_alertas");
+
 const limiteExplosividade = 1.9;
 
 const chart = new Chart(ctx, {
@@ -125,12 +123,80 @@ const chart = new Chart(ctx, {
         },
       },
       title: {
+        background: "#fff",
         display: true,
         text: "Concentração média por setor",
         position: "top",
         align: "start",
         font: {
           size: 20,
+          family: "Kanit",
+        },
+      },
+    },
+  },
+});
+
+const char2 = new Chart(ctx1, {
+  type: "bar",
+  data: {
+    labels: ["Setor A", "Setor B", "Setor C", "Setor D"],
+    datasets: [
+      {
+        label: "Quantidade de alertas totais",
+        data: [10, 6, 13, 8],
+        backgroundColor: "#36A2EB",
+        borderWidth: 1,
+      },
+      {
+        label: "Quantidade de alertas críticos (>= 1,9%)",
+        data: [4, 1, 2, 3],
+        backgroundColor: "#FF6384",
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          font: {},
+        },
+      },
+      title: {
+        display: true,
+        text: "Alertas por setor",
+        position: "top",
+        align: "center",
+        font: {
+          size: 20,
+        },
+      },
+    },
+    scales: {
+      y: {
+        title: {
+          display: true,
+          text: "Quantidade de alertas",
+          color: "#555",
+          font: { size: 12, weight: "600" },
+          padding: { bottom: 8 },
+        },
+        beginAtZero: true,
+      },
+      x: {
+        title: {
+          display: true,
+          text: "Setores",
+          color: "#555",
+          font: { size: 12, weight: "600" },
+          padding: { bottom: 8 },
+        },
+        ticks: {
+          font: {
+            weight: "bold",
+            family: "Kanit",
+          },
         },
       },
     },
